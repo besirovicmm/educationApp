d64f839f96d20aaf601c08ca791ea1ee524b48ae
Muhammed Besirovic <mbesirovic@creen.io>
2024-09-02 06:52:44 +0200

    Class seed

--- backend/knex/seeds/01_classes.js
+++ backend/knex/seeds/01_classes.js
@@ -3,12 +3,10 @@
  * @returns { Promise<void> } 
  */
 exports.seed = function(knex) {
-  // Deletes ALL existing entries
-  // return knex('classes').del()
-  //   .then(function () {
-  //     // Inserts seed entries
-      // return knex('classes').insert([
-      //   {id: 2, name: 'Second Class Name', description: 'Second Class'},
-      // ]);
-  //   });
+  return knex('classes').del()
+    .then(function () {
+      return knex('classes').insert([
+        {id: 1, name: 'Second Class Name', description: 'Second Class'},
+      ]);
+    });
 };
